322eb174afabaa887c4539d914a1cbe5dfb83a10
NetOperatorWibby <paul@dsgn.io>
2014-05-03 13:15:52 -0400

    Titlebar show functionality is now 10x better

--- resources/scripts/aries.js
+++ resources/scripts/aries.js
@@ -7,20 +7,41 @@
 
 	$(function () {
 
+		// Set URL bar width
 		$("#url-bar").css("width", $vW - 190 + "px");
 
-		$("#aries-powerbar").mouseover(function () {
-			$("#aries-titlebar").css({
-				"margin": "0",
-				"opacity": "1"
-			});
+		// Titlebar show functionality
+		var timeoutId;
+
+		$(".button-group").hover(function () {
+
+			if (!timeoutId) {
+				timeoutId = window.setTimeout(function () {
+
+					timeoutId = null; 
+
+					$("#aries-titlebar").css({
+						"margin": "0",
+						"opacity": "1"
+					});
+
+				}, 850);
+			}
+
 		});
 
-		$("#aries-showcase").mouseover(function () {
-			$("#aries-titlebar").css({
-				"margin": "0 0 -31px 0",
-				"opacity": "0"
-			});
+		$("#aries-showcase").hover(function () {
+
+			if (timeoutId) {
+				window.clearTimeout(timeoutId);
+				timeoutId = null;
+			} else {
+				$("#aries-titlebar").css({
+					"margin": "0 0 -31px 0",
+					"opacity": "0"
+				});
+			}
+
 		});
 
 	});
