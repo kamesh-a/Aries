b60e343369d149df0bb4777d40219d27785d6f37
NetOperatorWibby <paul@dsgn.io>
2014-05-04 15:14:40 -0400

    Guess what? Window controls work!

--- resources/scripts/aries.js
+++ resources/scripts/aries.js
@@ -45,3 +45,30 @@
 		});
 
 	});
+
+	// Window Actions!
+	$(function () {
+
+		var nw = {
+			gui: require("nw.gui"), // Load native UI library
+			win: require("nw.gui").Window.get(), // Get the current window
+			platform: require("os").platform,
+			spawn: require("child_process").spawn,
+			exec: require("child_process").exec,
+			fs: require("fs"),
+			path: require("path")
+		}
+
+	  $(".app-minimize").on("click", function () {
+			nw.win.minimize();
+		});
+
+	  $(".app-maximize").on("click", function () {
+			nw.win.maximize();
+		});
+
+	  $(".app-close").on("click", function () {
+			nw.win.close();
+		});
+
+	});
