498adcec677d9fe1b903e49bc60aacb57b2674ab
NetOperatorWibby <paul@dsgn.io>
2014-05-05 12:04:13 -0400

    Searches with spaces and URLs entered into URL bar work as expected

--- resources/scripts/aries.js
+++ resources/scripts/aries.js
@@ -10,6 +10,12 @@
 		// Set URL bar width
 		$("#url-bar").css("width", $vW - 190 + "px");
 
+		// Set showcase width and height
+		$("#aries-showcase").css({
+			"width": $vW,
+			"height": $vH - 31 + "px"
+		});
+
 		// Titlebar show functionality
 		var timeoutId;
 
@@ -72,6 +78,52 @@
 		// Close Aries
 		$(".app-close").on("click", function () {
 			nw.win.close();
+		});
+
+	});
+
+	function goThere() {
+
+		var reg = /\s+/;
+		var a = reg.test($("#url-bar").val());
+
+		if (reg.test($("#url-bar").val())) {
+
+			// if user inputs a space, it's indicative of a search
+			document.getElementById("aries-showcase").src = "http://duckduckgo.com?q=" + encodeURIComponent(document.getElementById("url-bar").value);
+			console.log(a); // should be true, search DDG
+
+		} else {
+
+			// if there are no spaces, it's indicative of a destination
+			document.getElementById("aries-showcase").src = "http://" + encodeURIComponent(document.getElementById("url-bar").value);
+			console.log(a); // should be false, go to actual site
+
+		}
+
+	}
+
+	$(function() {
+
+		"use strict";
+
+		var url = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+
+		$("#url-bar").on("keyup", function(e) {
+
+			var urls, output = "";
+
+			if (e.keyCode !== 8 && e.keyCode !== 9 && e.keyCode !== 13 && e.keyCode !== 32 && e.keyCode !== 46) {
+				// Return is backspace, tab, enter, space or delete was not pressed.
+				return;
+			}
+
+			while ((urls = url.exec(this.value)) !== null) {
+				output += urls[0] + ", ";
+			}
+
+			console.log("URLS: " + output.substring(0, output.length - 2));
+
 		});
 
 	});
